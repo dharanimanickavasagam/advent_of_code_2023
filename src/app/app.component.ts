11e74a9bc730225f0ff447fa6e65df877bc99605
Dharani <dhaaru1992@gmail.com>
2023-12-04 20:56:02 -0500

    day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advent_of_code';
+  file= 'app/fixtures/sample-input.txt';
+
+  fileData = null;
+  subs: Subscription | null = null;
+  summedValue: number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.subs = this.http.get('assets/sample-input.txt', { responseType: 'text' }).subscribe( {
+       next: (data: any) => {
+       this.parseFileData(data);
+       }, 
+       error: (error: any) => { 
+       }
+    });
+  }
+
+  parseFileData = (fileData: string) => {
+     const parsedCodes =  fileData.split(/\r?\n/);
+     const parsedNumbers: Array<number> =  [];
+
+     parsedCodes.map((code: string )=> { 
+        const reversedCode= code.split("").reverse().join("");
+        const firstDigitIndex: any = code.search(/\d+/);
+        const lastDigitIndex: any = reversedCode.search(/\d+/);
+        const parsedNumber = "" + parseInt(code[firstDigitIndex][0], 10) + parseInt(reversedCode[lastDigitIndex][0], 10)
+        parsedNumbers.push(parseInt(parsedNumber));
+     });
+     this.summedValue = parsedNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+
+  ngOnDestroy(){
+    this.subs?.unsubscribe();
+  }
 }
